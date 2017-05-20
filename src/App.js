8132174b3b07ca20e252c158d06892d8da508795
Reda Belahrach <reeda.bx@gmail.com>
2017-05-20 19:02:50 +0100

    say wuuuuuuuut

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import styles from './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={styles.AppIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
