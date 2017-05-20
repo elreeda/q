e6ea3b3bda0e6772b4e524aa7754ec9edc9451ef
Reda Belahrach <reeda.bx@gmail.com>
2017-05-20 14:48:27 +0100

    here we go again

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { browserHistory, Route, Router, IndexRoute} from 'react-router'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers/index'
-import Home from './components/Home'
-import App from './components/App'
-import ViewSurah from './components/surah/index'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home}></IndexRoute>
-        <Route path="/listen/:id" component={ViewSurah}></Route>
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
